4085d04a22914d8ed08067b091df30ae2b9ea17a
Eric Harrison <magicshadow818@gmail.com>
2024-03-13 23:33:15 +0000

    saving dump of database...

--- student_info.sh
+++ student_info.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+
 # Info about my computer science students from students database
+
 echo -e "\n~~ My Computer Science Students ~~\n"
 
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
+
 echo -e "\nFirst name, last name, and GPA of students with a 4.0 GPA:"
-echo "$($PSQL "select first_name,last_name,gpa from students where gpa = 4.0;")"
+echo "$($PSQL "SELECT first_name, last_name, gpa FROM students WHERE gpa = 4.0")"
 
 echo -e "\nAll course names whose first letter is before 'D' in the alphabet:"
+echo "$($PSQL "SELECT course FROM courses WHERE course < 'D'")"
+
+echo -e "\nFirst name, last name, and GPA of students whose last name begins with an 'R' or after and have a GPA greater than 3.8 or less than 2.0:"
+echo "$($PSQL "SELECT first_name,last_name,gpa FROM students WHERE last_name>='R' AND (gpa > 3.8 or gpa < 2.0)")"
+
+echo -e "\nLast name of students whose last name contains a case insensitive 'sa' or have an 'r' as the second to last letter:"
+echo "$($PSQL "SELECT last_name FROM students WHERE last_name ilike '%sa%' or last_name like '%r_'")"
+
+echo -e "\nFirst name, last name, and GPA of students who have not selected a major and either their first name begins with 'D' or they have a GPA greater than 3.0:"
