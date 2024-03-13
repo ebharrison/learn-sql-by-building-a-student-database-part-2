a89f666ba035c9023822f90b9abed8bd5244a583
Eric Harrison <magicshadow818@gmail.com>
2024-03-13 23:47:36 +0000

    saving dump of database...

--- student_info.sh
+++ student_info.sh
@@ -29,4 +29,6 @@ echo -e "\nAverage GPA of all students rounded to two decimal places:"
 echo "$($PSQL "select round(avg(gpa),2) from students;")"
 
 echo -e "\nMajor ID, total number of students in a column named 'number_of_students', and average GPA rounded to two decimal places in a column name 'average_gpa', for each major ID in the students table having a student count greater than 1:"
-echo "$($PSQL "select major_id, count(*) as 'number_of_students', avg(gpa,2) as 'average_gpa' where count(*)>1")"
+echo "$($PSQL "select major_id, count(*) as 'number_of_students', avg(gpa,2) as 'average_gpa' from students where count(*)>1")"
+
+# 1960
